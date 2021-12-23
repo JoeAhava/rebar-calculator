b080f8a65571f6dce3fa84393f065311e21b57ed
unknown <116eyosi@gmail.com>
2021-12-23 16:19:42 -0330

    added number fields and button UI | initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [valueL1, setValueL1] = useState(null);
+	const [valueQ1, setValueQ1] = useState(null);
+
+	const [valueL2, setValueL2] = useState(null);
+
+	const [valueQ2, setValueQ2] = useState(null);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div>
+					<input
+						type={"number"}
+						onChange={(e) => setValueL1(e.target.value)}
+						value={valueL1}
+						placeholder="Length 1"
+					/>
+					<input
+						type={"number"}
+						onChange={(e) => setValueQ1(e.target.value)}
+						value={valueQ1}
+						placeholder="Quantity 1"
+					/>
+				</div>
+				<div>
+					<input
+						type={"number"}
+						onChange={(e) => setValueL2(e.target.value)}
+						value={valueL2}
+						placeholder="Length 2"
+					/>
+					<input
+						type={"number"}
+						onChange={(e) => setValueQ2(e.target.value)}
+						value={valueQ2}
+						placeholder="Quantity 2"
+					/>
+				</div>
+				<button onClick={(e) => e.preventDefault()}>Calculate</button>
+				<p>
+					Min Number of rebars is <code>{0}</code>
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
